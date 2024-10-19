dbf81443820d3d8b1648c86251bf944933dd0e17
Ahmad Kholid <kholid060@gmail.com>
2024-10-19 17:19:33 +0800

    feat: add sidebar

--- components/dashboard/DashboardSidebar.vue
+++ components/dashboard/DashboardSidebar.vue
@@ -1,52 +1,87 @@
 <template>
   <aside
-    class="fixed top-0 flex h-screen w-64 flex-col bg-grass-12 p-4 text-olive-1"
+    class="fixed top-0 flex h-screen w-64 flex-col bg-grass-12 py-4 text-olive-1"
   >
-    <div class="grow"></div>
-    <UiDropdownMenu>
-      <UiDropdownMenuTrigger as-child>
-        <UiButton
-          variant="secondary"
-          class="h-12 w-full overflow-hidden px-2 text-left"
-        >
-          <UiAvatar class="size-8">
-            <UiAvatarImage :src="userStore.profile.avatarUrl ?? ''" />
-            <UiAvatarFallback>
-              <UserRoundIcon />
-            </UiAvatarFallback>
-          </UiAvatar>
-          <div class="ml-2 grow overflow-hidden pr-6 text-sm">
-            <p class="truncate">{{ userStore.profile.name }}</p>
-            <p class="truncate text-xs text-muted-foreground">
-              {{ userStore.profile.email }}
-            </p>
-          </div>
-          <ChevronDownIcon class="absolute right-6 size-5 shrink-0" />
-        </UiButton>
-      </UiDropdownMenuTrigger>
-      <UiDropdownMenuContent
-        class="dropdown-trigger-width text-sm"
-        side="top"
-        :side-offset="10"
+    <div class="mb-12 px-7 pt-4">
+      <h2 class="text-2xl font-bold">App name</h2>
+    </div>
+    <NuxtLink
+      v-for="link in sidebarLinks"
+      :key="link.to"
+      :to="link.to"
+      active-class="active"
+      class="sidebar-link group relative mb-3 px-4"
+    >
+      <span
+        class="indicator absolute left-0 top-0 hidden h-full w-1.5 rounded-r-full bg-primary"
+      ></span>
+      <p
+        class="flex items-center rounded-md p-2 text-olive-4 transition-colors hover:bg-white/10 hover:text-olive-1 group-hover:bg-white/10 group-hover:text-olive-1"
       >
-        <UiDropdownMenuItem
-          class="text-destructive focus:text-destructive"
-          @click="signOut"
+        <component :is="link.icon" class="mr-3 size-5" />
+        {{ link.label }}
+      </p>
+    </NuxtLink>
+    <div class="grow"></div>
+    <div class="px-4">
+      <UiDropdownMenu>
+        <UiDropdownMenuTrigger as-child>
+          <UiButton
+            variant="secondary"
+            class="h-12 w-full overflow-hidden px-2 text-left"
+          >
+            <UiAvatar class="size-8 bg-secondary-hover">
+              <UiAvatarImage :src="userStore.profile.avatarUrl ?? ''" />
+              <UiAvatarFallback>
+                <UserRoundIcon />
+              </UiAvatarFallback>
+            </UiAvatar>
+            <div class="ml-2 grow overflow-hidden pr-6 text-sm">
+              <p class="truncate">{{ userStore.profile.name }}</p>
+              <p class="truncate text-xs text-muted-foreground">
+                {{ userStore.profile.email }}
+              </p>
+            </div>
+            <ChevronDownIcon class="absolute right-6 size-5 shrink-0" />
+          </UiButton>
+        </UiDropdownMenuTrigger>
+        <UiDropdownMenuContent
+          class="dropdown-trigger-width text-sm"
+          side="top"
+          :side-offset="10"
         >
-          <LogOutIcon class="mr-2 size-4" />
-          Sign out
-        </UiDropdownMenuItem>
-      </UiDropdownMenuContent>
-    </UiDropdownMenu>
+          <UiDropdownMenuItem
+            class="text-destructive focus:text-destructive"
+            @click="signOut"
+          >
+            <LogOutIcon class="mr-2 size-4" />
+            Sign out
+          </UiDropdownMenuItem>
+        </UiDropdownMenuContent>
+      </UiDropdownMenu>
+    </div>
   </aside>
 </template>
 <script setup lang="ts">
-import { UserRoundIcon, LogOutIcon, ChevronDownIcon } from 'lucide-vue-next';
+import {
+  UserRoundIcon,
+  LogOutIcon,
+  ChevronDownIcon,
+  LinkIcon,
+  ChartColumnIcon,
+  SettingsIcon,
+} from 'lucide-vue-next';
 import { useToast } from '../ui/toast';
 
 const toast = useToast();
 const userStore = useUserStore();
 const supabase = useSupabaseClient();
+
+const sidebarLinks = [
+  { to: '/dashboard', icon: LinkIcon, label: 'Links' },
+  { to: '/dashboard/analytics', icon: ChartColumnIcon, label: 'Analytics' },
+  { to: '/dashboard/settings', icon: SettingsIcon, label: 'Settings' },
+];
 
 async function signOut() {
   const result = await supabase.auth.signOut();
@@ -62,3 +97,17 @@ async function signOut() {
   await navigateTo('/auth/login', { external: true });
 }
 </script>
+<style lang="postcss" scoped>
+.sidebar-link.active {
+  .indicator {
+    display: block;
+  }
+  p {
+    @apply bg-white/10;
+    color: theme('colors.olive.1');
+  }
+  svg {
+    color: theme('colors.primary.DEFAULT');
+  }
+}
+</style>
