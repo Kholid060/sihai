1d3a2402f7dca806f4941d1bd069718d5f2ea7df
Ahmad Kholid <kholid060@gmail.com>
2024-11-12 11:29:08 +0800

    feat: redirect app root

--- pages/index.vue
+++ pages/index.vue
@@ -7,4 +7,11 @@
 definePageMeta({
   name: 'landing-page',
 });
+
+const url = useRequestURL();
+const user = useSupabaseUser();
+
+if (url.hostname.startsWith('app.')) {
+  await navigateTo(user ? '/dashboard' : '/auth/signin', { replace: true });
+}
 </script>
