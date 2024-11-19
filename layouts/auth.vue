b5a6629ca58bad3b26704a5c0ea233a648f6caf9
Ahmad Kholid <kholid060@gmail.com>
2024-11-19 13:01:57 +0800

    feat: update auth layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex h-screen">
-    <div class="hidden w-full max-w-xl bg-grass-12 lg:block" />
+    <div class="hidden w-full max-w-xl content-end bg-grass-12 p-12 lg:block">
+      <h2 class="cursor-default text-3xl font-semibold text-primary">Sihai</h2>
+    </div>
     <div class="flex flex-1 items-center justify-center">
       <div class="w-full max-w-lg px-4">
         <slot />
